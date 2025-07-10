55985d19d8a542fe7b7b1c8d610af9e5695ed722
stamenkovicx <natasastamenkovic99@gmail.com>
2025-07-10 21:48:18 +0200

    feat: added username in comments

--- Explorer/src/app/feature-modules/post/model/comment.model.ts
+++ Explorer/src/app/feature-modules/post/model/comment.model.ts
@@ -3,12 +3,14 @@ export class Comment {
     content!: string;
     userId!: number;
     postId!: number;  // Samo ID posta, bez cele Post instance
+    username! : string;
 
 
     constructor() {
         this.userId = 0;
         this.postId = 0;
         this.content = '';
+        this.username = '';
     }
 }
 
